df3881d7102d7b6626d6f70445f4cc9d8a4d8966
Kelvin <alaricbaum94@gmail.com>
2017-11-25 12:35:03 -0800

    Changing spacing

--- entertainment_center.py
+++ entertainment_center.py
@@ -12,34 +12,34 @@ import media
 # please provide a clue as to why because they work perfectly on my end and I'm
 # rather curious as to what the problem may be with them.
 FIGHT_CLUB = media.Movie("Fight Club",
-                         "young men start a fight club",  #
-                         "https://i.ytimg.com/vi_webp/amiyHvl0I4s/movieposter.webp",
-                         "https://www.youtube.com/watch?v=SUXWAEX2jlg")
+    "young men start a fight club",  #
+    "https://i.ytimg.com/vi_webp/amiyHvl0I4s/movieposter.webp",
+    "https://www.youtube.com/watch?v=SUXWAEX2jlg")
 
 V_FOR_VENDETTA = media.Movie("V for Vendetta",
-                             "A shadowy freedom fighter",
-                             "https://i.ytimg.com/vi_webp/Et6M2r3yTAw/movieposter.webp",
-                             "https://www.youtube.com/watch?v=lSA7mAHolAw")
+    "A shadowy freedom fighter",
+    "https://i.ytimg.com/vi_webp/Et6M2r3yTAw/movieposter.webp",
+    "https://www.youtube.com/watch?v=lSA7mAHolAw")
 
 PULP_FICTION = media.Movie("Pulp Fiction",
-                           "Hit men converge in Quentin Tarantino's",
-                           "https://i.ytimg.com/vi_webp/hhMtO1MVFSo/movieposter.webp",
-                           "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+    "Hit men converge in Quentin Tarantino's",
+    "https://i.ytimg.com/vi_webp/hhMtO1MVFSo/movieposter.webp",
+    "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
 
 DONNIE_DARKO = media.Movie("Donnie Darko",
-                           "A troubled teenager is plagued",
-                           "https://i.ytimg.com/vi_webp/6ytjJM7RriQ/movieposter.webp",
-                           "https://www.youtube.com/watch?v=ZZyBaFYFySk")
+    "A troubled teenager is plagued",
+    "https://i.ytimg.com/vi_webp/6ytjJM7RriQ/movieposter.webp",
+    "https://www.youtube.com/watch?v=ZZyBaFYFySk")
 
 A_CLOCK_WORK_ORANGE = media.Movie("A Clock Work Orange",
-                                  "In future Britain, charismatic delinquent",
-                                  "https://i.ytimg.com/vi_webp/tzUR_CmoYLk/movieposter.webp",
-                                  "https://www.youtube.com/watch?v=SPRzm8ibDQ8")
+    "In future Britain, charismatic delinquent",
+    "https://i.ytimg.com/vi_webp/tzUR_CmoYLk/movieposter.webp",
+    "https://www.youtube.com/watch?v=SPRzm8ibDQ8")
 
 THE_DEVILS_REJECTS = media.Movie("The Devils Rejects",
-                                 "Ambushed at their isolated home by Sheriff",
-                                 "https://i.ytimg.com/vi_webp/RHDCwu1SEOg/movieposter.webp",
-                                 "https://www.youtube.com/watch?v=poxddLZq2k0")
+    "Ambushed at their isolated home by Sheriff",
+    "https://i.ytimg.com/vi_webp/RHDCwu1SEOg/movieposter.webp",
+    "https://www.youtube.com/watch?v=poxddLZq2k0")
 
 # Movies array, contains the list of movies as input, which
 # is passed to the function "open_movies_page". This function
